4b3682f7b9f0ef40586607bf6076e7e68774fade
Gino <ginoalodar@gmail.com>
2024-09-11 19:41:04 -0500

    fix: modified Footer to not show unused links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,47 +2,47 @@ import React from 'react'
 
 import {socialMedia} from '../constants';
 
-const FooterLink = () =>{
-  return (
-    <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-        <div className='flex flex-col items-center'>
-          <h2 className="font-bold text-xl mb-4">Nosotros</h2>
-          <ul>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Lorem ipsum dolor sit amet</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Consectetur adipiscing elit</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Sed do eiusmod tempor</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Incididunt ut labore</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Dolore magna aliqua</a></li>
-          </ul>
-        </div>
-        <div className='flex flex-col items-center'>
-          <h2 className="font-bold text-xl mb-4">Servicios</h2>
-          <ul>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Ut enim ad minim veniam</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Quis nostrud exercitation</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Ullamco laboris nisi</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Ut aliquip ex ea commodo</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Consequat</a></li>
-          </ul>
-        </div>
-        <div className='flex flex-col items-center'>
-          <h2 className="font-bold text-xl mb-4">Legal</h2>
-          <ul>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Excepteur sint occaecat</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Cupidatat non proident</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Sunt in culpa qui</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Officia deserunt mollit</a></li>
-            <li><a href="#" className="text-gray-400 hover:text-gray-300">Anim id est laborum</a></li>
-          </ul>
-        </div>
-      </div>
-      <div className="text-center mt-10">
-        <p className="text-bold">© 2024 PHES. Todos los derechos reservados.</p>
-      </div>
-    </div>
-  )
-}
+// const FooterLink = () =>{
+//   return (
+//     <div className="container mx-auto px-4">
+//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+//         <div className='flex flex-col items-center'>
+//           <h2 className="font-bold text-xl mb-4">Nosotros</h2>
+//           <ul>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Lorem ipsum dolor sit amet</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Consectetur adipiscing elit</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Sed do eiusmod tempor</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Incididunt ut labore</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Dolore magna aliqua</a></li>
+//           </ul>
+//         </div>
+//         <div className='flex flex-col items-center'>
+//           <h2 className="font-bold text-xl mb-4">Servicios</h2>
+//           <ul>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Ut enim ad minim veniam</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Quis nostrud exercitation</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Ullamco laboris nisi</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Ut aliquip ex ea commodo</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Consequat</a></li>
+//           </ul>
+//         </div>
+//         <div className='flex flex-col items-center'>
+//           <h2 className="font-bold text-xl mb-4">Legal</h2>
+//           <ul>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Excepteur sint occaecat</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Cupidatat non proident</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Sunt in culpa qui</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Officia deserunt mollit</a></li>
+//             <li><a href="#" className="text-gray-400 hover:text-gray-300">Anim id est laborum</a></li>
+//           </ul>
+//         </div>
+//       </div>
+//       <div className="text-center mt-10">
+//         <p className="text-bold">© 2024 PHES. Todos los derechos reservados.</p>
+//       </div>
+//     </div>
+//   )
+// }
 
 const Footer = () => {
   return (
@@ -56,8 +56,11 @@ const Footer = () => {
                 </h2>
               </div>
               
-              <div className='flex flex-grow justify-between 2xl:justify-end p-6'>
+              {/* <div className='flex flex-grow justify-between 2xl:justify-end p-6'>
                 <FooterLink />
+              </div> */}
+              <div className="text-center text-xl font-bold">
+                <p className="text-bold">© 2024 PHES. Todos los derechos reservados.</p>
               </div>
               
                 <div className="flex flex-row justify-between items-center md:mt-0 mt-2">
